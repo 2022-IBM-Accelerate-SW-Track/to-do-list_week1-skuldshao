7df94660dfc768932f0c20294a2111c4933eb19f
Skuld Shao <77602197+skuldshao@users.noreply.github.com>
2022-06-08 21:28:23 -0700

    week1lab

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -38,19 +38,21 @@ class AddTodo extends Component {
       // an OnClick event.
       // 3. The value of the text field also should reflect the local state of this component.
       <div>
-        <div data-testid="new-item-input"/>;
+
         <TextField
           label="Add New Item"
           variant="outlined"
           onChange={this.handleChange}
           value={this.state.content}
+          data-testid="new-item-input"
         />
-        <div data-testid="new-item-button"/>;
+     
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
           variant="contained"
           color="primary"
+          data-testid="new-item-button"
           
         >
           Add
